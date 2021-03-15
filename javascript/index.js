578c0cc24f959a48fff480b4fe56660a5b0b5a07
JoseLopezGallego <joseanlopez61@gmail.com>
2021-03-15 17:57:05 +0100

    fichier produit.js

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,8 @@ let produits;//Variable qui va stocker les données récuperées dès l'API
 
 //API REQUEST avec des fonctions fléchéés pour récuperer les produits souhaités
 const fetchProduits = async() => {
-    produits = await fetch('http://localhost:3000/api/cameras').then(res => res.json());
+    produits = await fetch('http://localhost:3000/api/cameras').then((res) => res.json());
+    console.log(produits);
 };
 
 /* La fonction précédent est équivalente à :
@@ -22,16 +23,25 @@ const displayProduits = async() => {
             .map(produit => (
                 `
                     <li class="produit-item"><a href="produit.html">
+                        <div class="produit-item__id">${produit._id}</div>
                         <img class="produit-item__img" src="${produit.imageUrl}" />
                         <h3 class="produit-item__name">${produit.name}<h3>
                         <p class="produit-item__price">Prix : ${numberWithSpace(produit.price)} €</p>
-                        <button onclick="window.open('http://127.0.0.1:5501/produit.html', '_self')" class="produit-item__bouton" id="btn">Plus de détails</button>
+                        <button onclick="test(event,${produit})" class="produit-item__bouton">Plus de détails</button>
                     </a></li>
                 `           
                 )
             ).join('')
-    )
+    );
+
+    let btn = document.getElementById("test")
 };
+
+function test(e,produit) {
+    e.preventDefault();
+    console.log(produit);
+    window.location.href =`produit.html?id=${produit._id}`
+}
 
 displayProduits(); //Pour afficher les résultats de la recherche
 
@@ -52,12 +62,12 @@ Btn.addEventListener('click', () =>{
 })*/
 
 
-const appareil = document.getElementById('appareil');
+/*const appareil = document.getElementById('appareil');
 
 const firstCamera = 'camera[0]';
 const secondCamera = 'camera[1]';
 const thirdCamera = 'camera[2]';
-const fourthdCamera = 'camera[3]';
+const fourthCamera = 'camera[3]';
 const fifthCamera = 'camera[4]';
 
 let cameras;
@@ -75,17 +85,19 @@ const showCameras = async() => {
                 `
                     <li class="cameras-item">
                         <img class="cameras-item__img" src="${cameras.imageUrl}" />
-                        <h3 class="cameras-item__name">${cameras.name}<h3>
-                        <p class="cameras-item__descr">${cameras.description}</p>
-                        <label class="cameras-item__lens">Lentilles
-                            <select>
-                                <option>${cameras.lenses[0]}</option>
-                                <option>${cameras.lenses[1]}</option>
-                                <option>${cameras.lenses[2]}</option>
-                            </select>
-                        </label>
-                        <p class="cameras-item__price">Prix : ${numberWithSpace(cameras.price)} €</p>
-                        <button onclick="window.open('http://127.0.0.1:5501/produit.html', '_self')" class="cameras-item__bouton" id="btn">Ajouter au panier</button>
+                        <form method="post" action=""> 
+                            <h3 class="cameras-item__name">${cameras.name}<h3>
+                            <p class="cameras-item__descr">${cameras.description}</p>
+                            <label class="cameras-item__lens">Lentilles
+                                <select>
+                                    <option>${cameras.lenses[0]}</option>
+                                    <option>${cameras.lenses[1]}</option>
+                                    <option>${cameras.lenses[2]}</option>
+                                </select>
+                            </label>
+                            <p class="cameras-item__price">Prix : ${numberWithSpace(cameras.price)} €</p>
+                            <button onclick="window.open('http://127.0.0.1:5501/produit.html', '_self')" class="cameras-item__bouton">Ajouter au panier</button>
+                        </form>
                     </li>
                 `           
                 )
@@ -94,4 +106,4 @@ const showCameras = async() => {
 };
 
 
-showCameras(firstCamera.slice[0]);
+showCameras();*/
